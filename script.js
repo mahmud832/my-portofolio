e3c2222d0d35d495883ae4912f665c9df0f9884e
mahmud <mahmudganteng832@gmail.com>
2026-07-31 20:11:00 +0700

    website hotel
    website hotel

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-// Smooth Scroll
-document.querySelectorAll("nav a").forEach(link => {
-    link.addEventListener("click", function(e){
-        e.preventDefault();
-
-        const tujuan = document.querySelector(this.getAttribute("href"));
-
-        tujuan.scrollIntoView({
-            behavior: "smooth"
-        });
-    });
-});
+document.querySelector('button').onclick=()=>alert('Booking demo');
